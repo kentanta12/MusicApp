257f691da07a7cc7d60ec0ec633b33f39de69a62
KentaNiki <kentanta@113x33x194x194.ap113.ftth.ucom.ne.jp>
2020-07-06 16:55:31 +0900

    create MyMusicproject

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by KentaNiki on 2020/07/06.
+//  Copyright © 2020 KentaNiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
