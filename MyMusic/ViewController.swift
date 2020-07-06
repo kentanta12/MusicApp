dc8b1977bf44d0791a5e471fc2bd8fee75e61571
KentaNiki <kentanta@113x33x194x194.ap113.ftth.ucom.ne.jp>
2020-07-06 22:46:55 +0900

    refactoring

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -23,14 +23,7 @@ class ViewController: UIViewController {
     var cymbalPlayer = AVAudioPlayer()
 
     @IBAction func cymbal(_ sender: Any) {
-        do {
-            // シンバル用のプレイヤーに、音源ファイル名を指定
-            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
-            //シンバルの音源再生
-            cymbalPlayer.play()
-        } catch {
-            print("シンバルで、エラーが発生しました！")
-        }
+        soundPlayer(player: &cymbalPlayer ,path:cymbalPath, count: 0)
     }
     // ギターの音源ファイルを設定
     let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
@@ -39,13 +32,7 @@ class ViewController: UIViewController {
     var guitarPlayer = AVAudioPlayer()
     
     @IBAction func guitar(_ sender: Any) {
-        do {
-            // ギター用のプレイヤーに、音響ファイル名を指定
-            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
-            guitarPlayer.play()
-        } catch {
-            print("ギターで、エラーが発生しました！")
-        }
+        soundPlayer(player: &guitarPlayer ,path:guitarPath, count:0)
     }
     // バックミュージックの音源ファイルを指定
     let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
@@ -55,21 +42,24 @@ class ViewController: UIViewController {
     
     // Playボタンがタップされた時の処理
     @IBAction func play(_ sender: Any) {
-        do {
-            // バックミュージック用のプレイヤーに、音響ファイル名を指定
-            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
-            // リピート設定
-            backmusicPlayer.numberOfLoops = -1
-            backmusicPlayer.play()
-        } catch {
-            print("エラーが発生しました！")
-        }
+        soundPlayer(player: &backmusicPlayer ,path:backmusicPath, count: -1)
     }
     
     // Stopボタンがタップされた時の処理
     @IBAction func stop(_ sender: Any) {
         // バックミュージック停止
         backmusicPlayer.stop()
+    }
+    
+    // 共通のプレイヤー再生処理
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch{
+            print("エラーが発生しました!")
+        }
     }
     
 }
